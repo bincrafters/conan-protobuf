aa230e9479e90532f2a631889a9cea4f450e2e19
Uilian Ries <uilianries@gmail.com>
2018-04-12 18:46:22 -0300

    Build libstdc++11
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -6,11 +6,10 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
-    
+    builder = build_template_default.get_builder(pure_c=False)
+
     # Todo: re-enable shared builds when issue resolved
     # github issue: https://github.com/google/protobuf/issues/2502
     builder.items = filter(lambda build: build.options["protobuf:shared"] == False, builder.items)
 
     builder.run()
-    
