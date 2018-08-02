10d97348e46af3adfd13042cafb0d692bc3b1d57
SSE4 <tomskside@gmail.com>
2018-08-02 12:49:03 +0700

    - run otool to inspect output binary file
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -20,10 +20,11 @@ class TestPackageConan(ConanFile):
 
     def test(self):
         with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "test_package")
+            bin_path = os.path.abspath(os.path.join("bin", "test_package"))
             if self.settings.os == "Windows":
                 self.run(bin_path)
             elif self.settings.os == "Macos":
+                self.run("otool -L %s" % bin_path)
                 self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
                 self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
