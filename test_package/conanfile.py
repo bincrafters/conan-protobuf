c4f8eea503c0e15a4039b6a098b1333b13cb9c6a
SSE4 <tomskside@gmail.com>
2018-08-03 15:57:35 +0700

    - additional logging
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -25,6 +25,15 @@ class TestPackageConan(ConanFile):
                 self.run(bin_path)
             elif self.settings.os == "Macos":
                 self.run("otool -L %s" % bin_path)
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+                # https://developer.apple.com/library/archive/documentation/DeveloperTools/Conceptual/DynamicLibraries/100-Articles/LoggingDynamicLoaderEvents.html
+                debug_info = "DYLD_PRINT_LIBRARIES=1 " \
+                             "DYLD_PRINT_LIBRARIES_POST_LAUNCH=1 " \
+                             "DYLD_PRINT_APIS=1 " \
+                             "DYLD_PRINT_STATISTICS=1 " \
+                             "DYLD_PRINT_INITIALIZERS=1 " \
+                             "DYLD_PRINT_SEGMENTS=1 " \
+                             "DYLD_PRINT_BINDINGS=1"
+                self.run("%s DYLD_LIBRARY_PATH=%s %s" % (debug_info, os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
             else:
+
                 self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
