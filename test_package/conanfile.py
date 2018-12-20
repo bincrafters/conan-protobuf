d43d322abe37f1de12c6bf69280504240201956c
Uilian Ries <uilianries@gmail.com>
2018-12-20 15:47:35 -0200

    Remove protoc support
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,17 +8,15 @@ from conans import ConanFile, CMake, tools, RunEnvironment
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    requires = "protoc_installer/3.6.1@bincrafters/stable"
 
     def build(self):
         with tools.environment_append(RunEnvironment(self).vars):
-            cmake = CMake(self, set_cmake_flags=True)
+            cmake = CMake(self, parallel=False)
             cmake.definitions["OPTIMIZED_FOR"] = "LITE_RUNTIME" if self.options["protobuf"].lite else "SPEED"
-            cmake.definitions["ENABLE_PROTOC"] = not self.options["protobuf"].lite
             cmake.configure()
             cmake.build()
 
     def test(self):
         bin_path = os.path.abspath(os.path.join("bin", "test_package"))
         self.run(bin_path, run_environment=True)
-        if not self.options["protobuf"].lite:
-            self.run("protoc --version", run_environment=True)
