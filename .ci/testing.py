3e080a7345efa924e7b7fa011e3e893090ec1666
Uilian Ries <uilianries@gmail.com>
2018-12-27 10:15:32 -0200

    Trigger integration test
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- .ci/testing.py
+++ .ci/testing.py
@@ -11,7 +11,11 @@ if __name__ == "__main__":
     manager = CIManager(Printer())
     response = None
     if ci_manager.is_travis():
-        if manager.get_branch() == "stable/3.6.1" and not manager.is_pull_request() and os.getenv("CONAN_CLANG_VERSIONS") == "6.0" and os.getenv("CONAN_ARCHS") == "x86_64":
+        if manager.get_branch() == "stable/3.6.1" and \
+           not manager.is_pull_request() and \
+           os.getenv("CONAN_CLANG_VERSIONS") == "6.0" and \
+           os.getenv("CONAN_ARCHS") == "x86_64" and \
+           os.getenv("TRAVIS_TOKEN"):
             json_data = {"request": {"branch": "release/3.6.1"}}
             headers = {"Authorization": "token %s" % os.getenv("TRAVIS_TOKEN"), "Travis-API-Version": "3"}
             response = requests.post(url="https://api.travis-ci.com/repo/bincrafters%2Fprotobuf-integration-test/requests", json=json_data, headers=headers)
@@ -21,8 +25,13 @@ if __name__ == "__main__":
             print("PULL REQUEST: %s" % manager.is_pull_request())
             print("CLANG_VERSIONS: %s" % os.getenv("CONAN_CLANG_VERSIONS"))
             print("ARCHS: %s" % os.getenv("CONAN_ARCHS"))
+            print("TOKEN: %s" % (True if os.getenv("TRAVIS_TOKEN") else None))
     elif ci_manager.is_appveyor():
-        if manager.get_branch() == "stable/3.6.1" and not manager.is_pull_request() and os.getenv("CONAN_VISUAL_VERSIONS") == "15" and os.getenv("CONAN_BUILD_TYPES") == "Debug":
+        if manager.get_branch() == "stable/3.6.1" and \
+           not manager.is_pull_request() and \
+           os.getenv("CONAN_VISUAL_VERSIONS") == "15" and \
+           os.getenv("CONAN_BUILD_TYPES") == "Debug" and \
+           os.getenv("APPVEYOR_TOKEN"):
             json_data = {"accountName":"BinCrafters", "projectSlug": "protobuf-integration-test", "branch": "release/3.6.1"}
             headers = {"Authorization": "Bearer %s" % os.getenv("APPVEYOR_TOKEN")}
             response = requests.post(url="https://ci.appveyor.com/api/builds", json=json_data, headers=headers)
@@ -32,6 +41,7 @@ if __name__ == "__main__":
             print("PULL REQUEST: %s" % manager.is_pull_request())
             print("VISUAL VERSIONS: %s" % os.getenv("CONAN_VISUAL_VERSIONS"))
             print("BUILD_TYPES: %s" % os.getenv("CONAN_BUILD_TYPES"))
+            print("TOKEN: %s" % (True if os.getenv("APPVEYOR_TOKEN") else None))
     else:
         print("WARNING: No CI manager detected")
 
